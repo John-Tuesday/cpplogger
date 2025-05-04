e594fedcf2708da920eaca5aaad7a5a0b57d4a74
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2025-05-04 05:27:44 -0500

    Add documentation also remove unsed type alias

--- src/CppLogger/concepts.hpp
+++ src/CppLogger/concepts.hpp
@@ -9,22 +9,34 @@ namespace logger {
 enum class MessageType;
 }
 
+/**
+ * @brief Constraints for logging helper classes.
+ */
 namespace logger::concepts {
 
+/** Callable that returns a usable output stream. */
 template <typename T>
 concept ProvidesLogTarget = requires(T t) {
   { t() } -> std::common_reference_with<std::ostream &>;
 };
 
+/** Range of objects with constraint `ProvidesLogTarget`. */
 template <typename T>
 concept LogTargetProviderRange =
     std::ranges::range<T> && ProvidesLogTarget<std::ranges::range_value_t<T>>;
 
+/** Provides a range of log target provides. */
 template <typename T, MessageType M>
 concept IndirectlyProvidesLogTargets = requires(T t) {
   { t.template logproviders<M>() } noexcept -> LogTargetProviderRange;
 };
 
+/**
+ * Meets the requirements of a log formatter.
+ *
+ * Should return iterator past the end of the output range.
+ * Should essentially mirror the behavior of `std::format_to`.
+ */
 template <typename T, MessageType M>
 concept IsLogFormatter = requires(T t) {
   {
@@ -34,6 +46,7 @@ concept IsLogFormatter = requires(T t) {
   } -> std::same_as<decltype(std::ostream_iterator<char>(std::cout))>;
 };
 
+/** Meets the requirements of a log filter. */
 template <typename T, MessageType M>
 concept IsLogFilter = requires(T t) {
   {
--- src/CppLogger/logger.hpp
+++ src/CppLogger/logger.hpp
@@ -10,38 +10,65 @@
 #include <string_view>
 #include <syncstream>
 
+/** Base namespace. */
 namespace logger {
 
+/** Fundamental log message types. */
 enum class MessageType {
+  /** Most severe. */
   Fatal,
+  /** Less severe than `Fatal`. */
   Error,
+  /** Less severe than `Error`. */
   Warning,
+  /** Less severe than `Warning`. */
   Info,
+  /** Less severe than `Info`. */
   Debug,
+  /** Less severe than `Debug`. */
   Verbose,
 };
 
 template <typename... Args> struct LogFormatString;
 
 template <typename> struct LogTargets;
+/** Used as the template argument for the default `LogTargets`. */
 struct DefaultProviderTag {};
 template <typename> struct LogFormatter;
+/** Used as the template argument for the default `LogFormatter`. */
 struct DefaultFormatterTag {};
 template <typename> struct LogFilter;
+/** Used as the template argument for the default `LogFilter`. */
 struct DefaultLogFilterTag {};
 
 template <MessageType> struct MessageTypeTraits;
 
+/** Format string with extra information useful when logging. */
 template <typename... Args>
 struct LogFormatString
     : public std::format_string<std::type_identity_t<Args>...> {
-  using ParentType = std::format_string<std::type_identity_t<Args>...>;
 
+  /**
+   * Forwards `fmt` to `std::format_string`.
+   *
+   * @note Generally, no input should be given for `location`; this way the
+   * `std::source_location` will be constructed with respect to the callsite.
+   *
+   * @param[in] fmt forwarded as the first argument of the constructor for
+   * `std::format_string<>`
+   * @param[in] location location information to associated with this log
+   * message
+   */
   template <typename T>
   consteval LogFormatString(
       T &&fmt, std::source_location location = std::source_location::current())
-      : ParentType(std::forward<T>(fmt)), m_location(location) {}
+      : std::format_string<>(std::forward<T>(fmt)), m_location(location) {}
 
+  /**
+   * Source location information associated with the format string.
+   *
+   * @return associated location
+   */
   constexpr std::source_location location() const noexcept {
     return m_location;
   }
@@ -50,7 +77,19 @@ private:
   std::source_location m_location{};
 };
 
+/**
+ * Injectable type which should satisfy
+ * `logger::concepts::IndirectlyProvidesLogTargets`.
+ *
+ * Specialize for `DefaultProviderTag` to define custom behavior.
+ */
 template <typename T> struct LogTargets {
+
+  /**
+   * @return a range of target providers to be used for a given `MessageType`.
+   *
+   * Default implementation returns an empty view.
+   */
   template <MessageType M>
   auto logproviders() const noexcept -> decltype(auto) {
     return std::ranges::empty_view<decltype([]() -> std::ostream & {
@@ -59,32 +98,89 @@ template <typename T> struct LogTargets {
   }
 };
 
+/**
+ * Base implementation of a log formatter that satisfies
+ * `logger::concepts::IsLogFormatter`.
+ *
+ * By default, an object of this type with template argument
+ * `DefaultFormatterTag` is used as the formatter type for logging. Custom
+ * behavior can be provided by specializing that type.
+ */
 template <typename T> struct LogFormatter {
+
+  /**
+   * Formats a log message to an output iterator.
+   *
+   * By default, wraps `std::format_to`.
+   *
+   * @param[in] out iterator to the output buffer
+   * @param[in] msg formatted log input message
+   * @return iterator past the end of the output range
+   */
   template <MessageType M, std::output_iterator<char> OutputIt>
-  OutputIt format(const std::source_location &, OutputIt out,
-                  std::string_view msg) const noexcept {
+  OutputIt
+  format(const std::source_location & /**< [in] log message position */,
+         OutputIt out, std::string_view msg) const noexcept {
     return std::format_to(std::move(out), "{}", msg);
   }
 };
 
+/**
+ * Base implementation of a log filter that satisfies
+ * `logger::concepts::IsLogFilter`.
+ *
+ * Specialize this type for template argument `DefaultLogFilterTag` to define
+ * custom default filtering.
+ */
 template <typename T> struct LogFilter {
+
+  /**
+   * Determine if logging should be skipped for a particular `MessageType` and
+   * `std::source_location`.
+   *
+   * Default implementation always return `true`.
+   *
+   * @return `true` if the message should be logged, `false` otherwise.
+   */
   template <MessageType M>
-  bool filter(const std::source_location &) const noexcept {
+  bool filter(const std::source_location
+                  & /**< Source location of the log message */) const noexcept {
     return true;
   }
 };
 
+/**
+ * Provides the default types used for logging a message of a given type.
+ */
 template <MessageType M> struct MessageTypeTraits {
+
+  /** `MessageType` to which these traits belong */
   static constexpr MessageType type = M;
 
+  /** Provider of log targets */
   using TargetProvider = LogTargets<DefaultProviderTag>;
+
+  /** Formatter of log messages */
   using Formatter = LogFormatter<DefaultFormatterTag>;
+
+  /** Filter of log messages */
   using Filter = LogFilter<DefaultLogFilterTag>;
+
   static_assert(concepts::IndirectlyProvidesLogTargets<TargetProvider, M>);
   static_assert(concepts::IsLogFormatter<Formatter, M>);
   static_assert(concepts::IsLogFilter<Filter, M>);
 };
 
+/**
+ * Base logging implementation.
+ *
+ * @tparam type describes the type of log message
+ * @tparam Provider provides a range of log target providers
+ * @tparam Formatter called to format the log output
+ * @tparam Filter called to determine if this call to log should be skipped
+ * @param fmt log message format string
+ * @param args arguments to be formatted
+ */
 template <MessageType type,
           concepts::IndirectlyProvidesLogTargets<type> Provider =
               MessageTypeTraits<type>::TargetProvider,
@@ -105,36 +201,67 @@ void log(LogFormatString<std::type_identity_t<Args>...> fmt,
   }
 }
 
+/**
+ * Log using defaults for `MessageType::Fatal`.
+ *
+ * @param[in] fmt format string input
+ * @param[in] args variables to formated
+ */
 template <typename... Args>
 void logfatal(LogFormatString<std::type_identity_t<Args>...> fmt,
               Args &&...args) noexcept {
   log<MessageType::Fatal>(std::move(fmt), std::forward<Args>(args)...);
 }
 
+/**
+ * Log using defaults for `MessageType::Error`.
+ *
+ * @copydetails logfatal()
+ */
 template <typename... Args>
 void logerror(LogFormatString<std::type_identity_t<Args>...> fmt,
               Args &&...args) noexcept {
   log<MessageType::Error>(std::move(fmt), std::forward<Args>(args)...);
 }
 
+/**
+ * Log using defaults for `MessageType::Warning`.
+ *
+ * @copydetails logfatal()
+ */
 template <typename... Args>
 void logwarn(LogFormatString<std::type_identity_t<Args>...> fmt,
              Args &&...args) noexcept {
   log<MessageType::Warning>(std::move(fmt), std::forward<Args>(args)...);
 }
 
+/**
+ * Log using defaults for `MessageType::Info`.
+ *
+ * @copydetails logfatal()
+ */
 template <typename... Args>
 void loginfo(LogFormatString<std::type_identity_t<Args>...> fmt,
              Args &&...args) noexcept {
   log<MessageType::Info>(std::move(fmt), std::forward<Args>(args)...);
 }
 
+/**
+ * Log using defaults for `MessageType::Debug`.
+ *
+ * @copydetails logfatal()
+ */
 template <typename... Args>
 void logdebug(LogFormatString<std::type_identity_t<Args>...> fmt,
               Args &&...args) noexcept {
   log<MessageType::Debug>(std::move(fmt), std::forward<Args>(args)...);
 }
 
+/**
+ * Log using defaults for `MessageType::Verbose`.
+ *
+ * @copydetails logfatal()
+ */
 template <typename... Args>
 void logverbose(LogFormatString<std::type_identity_t<Args>...> fmt,
                 Args &&...args) noexcept {
