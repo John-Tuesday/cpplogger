445f0e48c53cfaf427df42ccd8d0e0fdbbed2dce
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2025-07-23 18:18:24 -0500

    Remove cryptic tests and fixtures

--- tests/auto/sanedefaults.cpp
+++ tests/auto/sanedefaults.cpp
@@ -35,37 +35,11 @@ concept LogHelper = (sizeof...(CharTs) >= 1) &&
                       logger::concepts::PrintsToLog<T, CharTs>) &&
                      ...);
 
-void runTest() {
-  {
-    static_assert(LogHelper<logger::TemplLogger, char>);
-    static_assert(
-        LogHelper<logger::LoggerDefaults<logger::DefaultImplTag>, char>);
-  }
-  using Cxt = logger::MTypeContext<logger::MessageType::Info, char>;
-  {
-    logger::LoggerDefaults<logger::DefaultImplTag> deflog{};
-    deflog.log<Cxt>("Def log: {}", 5);
-  }
-  {
-    logger::TemplLogger temlog{};
-    temlog.log<logger::MTypeContext<logger::MessageType::Debug, char>>(
-        "Templ log(Debug): {}", 5);
-    temlog.log<Cxt>("Templ log(default): {}", 5);
-  }
-  {
-    logger::LoggerDefaults<logger::DefaultImplTag> injLog{};
-    injLog.log<Cxt>("Inj log: {}", 5);
-    injLog.log<logger::MTypeContext<logger::MessageType::Debug, wchar_t>>(
-        L"Inj log: {}", 5);
-  }
-  std::println("finished ctx!");
-}
-
 } // namespace test::ctx
 
 int main() {
   std::println("\nBegin: {}\n", "sanedefaults");
-  test::ctx::runTest();
+  logger::test::verifyLoggerCls();
   std::println("\nEnd: {}\n", "sanedefaults");
   return 0;
 }
