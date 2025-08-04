30e8dc3e698d75a29c2322347d6fd7e22ce12beb
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2025-08-03 19:29:28 -0500

    Remove test of base functions from TermColor tests

--- tests/TermColor/TermColorLoggerUnitTest.cpp
+++ tests/TermColor/TermColorLoggerUnitTest.cpp
@@ -1,12 +1,5 @@
 #include <CppLogger/TermColorLogger.hpp>
 
-static_assert(logger::isFatal(logger::FatalContext<char>{}));
-static_assert(logger::isError(logger::ErrorContext<char>{}));
-static_assert(logger::isWarning(logger::WarnContext<char>{}));
-static_assert(logger::isInfo(logger::InfoContext<char>{}));
-static_assert(logger::isDebug(logger::DebugContext<char>{}));
-static_assert(logger::isVerbose(logger::VerboseContext<char>{}));
-
 int main() {
   logger::TermColorLogger logger{};
   logger.log<logger::FatalContext<char>>("log message for {}", "Fatal");
