f26849e810de7fa489fd45fc3c19ffca17de3627
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2026-02-02 22:55:18 -0600

    Refactor test for better automated testing
    Fixed bug where tests that contained the expected output would fail.
    Returns appropriate value for success/fail.

--- tests/TestFileLog.cpp
+++ tests/TestFileLog.cpp
@@ -1,5 +1,3 @@
-#include "TestFileLog.hpp"
-
 #include "tests/Fixtures/DefaultImpl.hpp"
 
 #include <cpplogger/logger.hpp>
@@ -7,31 +5,43 @@
 #include <cassert>
 #include <string_view>
 
-void logger::test::TestFileLog::test() {
+namespace logger::test {
+
+bool testFileLog();
+
+}
+
+int main() {
+  if (!logger::test::testFileLog())
+    return 1;
+  return 0;
+}
+
+/**
+ * Log to a logger which writes to a file, then verify the contents of the file.
+ *
+ * TODO: Find a better to communicate reasons for failure
+ *
+ * TODO: Ensure log file is empty before writing to log.
+ * TODO: Ensure log file contains ONLY what is expected and nothing else.
+ */
+bool logger::test::testFileLog() {
   using Context = logger::InfoContext<char>;
   constexpr std::string_view expect = "info: 5 == 5";
   logger::test::LogTargetsBasicFileLog logger{};
   logger.log<Context>("info: 5 == {}", 5);
   std::ifstream logIn{logger::test::LogTargetsBasicFileLog::logPath()};
-  bool noLineRead{true};
   for (std::string line; std::getline(logIn, line, '\n');) {
-    noLineRead = false;
-    if (line.contains(expect)) {
+    if (!line.contains(expect)) {
       std::println("!!!FAIL!!!");
       std::println("  expect: '{}'", expect);
       std::println("  actual: '{}'", line);
+      return false;
     } else {
       std::println("PASS");
+      return true;
     }
   }
-  assert(!noLineRead);
-}
-
-void logger::test::testFileLog() {
-  TestFileLog{}.test();
-}
-
-int main() {
-  logger::test::testFileLog();
-  return 0;
+  // TODO: Communicate that this failed because no line was read
+  return false;
 }
