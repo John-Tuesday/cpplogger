a98605c424a283e546e81ec8441b5b941939e2d0
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2026-03-09 22:36:17 -0500

    Simplify test logger

--- tests/TestFileLog.cpp
+++ tests/TestFileLog.cpp
@@ -7,7 +7,6 @@
 
 #include <fstream>
 #include <source_location>
-#include <sstream>
 #include <string_view>
 
 namespace cpplogger::test {
@@ -23,7 +22,7 @@ cpplogger::test::Result testFileLog(
 
 int main() {
   if (auto result =
-          cpplogger::test::testFileLog<cpplogger::InfoContext>("info: 5 == 5");
+          cpplogger::test::testFileLog("5 == 5", cpplogger::InfoContext());
       !result)
     return cpplogger::test::printError(result.error());
   return 0;
@@ -33,22 +32,17 @@ class cpplogger::test::LogTargetsBasicFileLog
     : public cpplogger::BasicLogger<char> {
 
  public:
-  LogTargetsBasicFileLog();
-
   template <typename Context, typename... Args>
   void
   log(Context&& context,
       std::format_string<std::type_identity_t<Args>...> fmt,
       Args&&... args) {
-    std::ofstream logStream = getLogStream();
+    std::ofstream logStream = {
+        getLogPath(),
+        std::ios_base::out | std::ios_base::trunc};
     std::ostreambuf_iterator fout{logStream};
     formatTo(
         fout,
-        std::forward<Context>(context),
-        fmt,
-        std::forward<Args>(args)...);
-    formatTo(
-        std::ostreambuf_iterator{std::cerr},
         std::forward<Context>(context),
         fmt,
         std::forward<Args>(args)...);
@@ -61,24 +55,9 @@ class cpplogger::test::LogTargetsBasicFileLog
   const std::filesystem::path& getLogPath() const { return m_logPath; }
 
  private:
-  std::filesystem::path m_logPath{};
-
-  /**
-   * Opens log file for ouput.
-   *
-   * The file is created if it does not exist and the contents of the file are
-   * cleared if it does.
-   */
-  std::ofstream getLogStream() const;
+  std::filesystem::path m_logPath = {
+      *cpplogger::test::tempDirectory() / getLogFileName()};
 };
-
-cpplogger::test::LogTargetsBasicFileLog::LogTargetsBasicFileLog() {
-  m_logPath = *cpplogger::test::tempDirectory() / getLogFileName();
-}
-
-std::ofstream cpplogger::test::LogTargetsBasicFileLog::getLogStream() const {
-  return std::ofstream{getLogPath(), std::ios_base::out | std::ios_base::trunc};
-}
 
 /**
  * Log to a logger which writes to a file, then verify the contents of the file.
@@ -87,10 +66,8 @@ template <typename Context>
 cpplogger::test::Result
 cpplogger::test::testFileLog(std::string_view input, Context context) {
   cpplogger::test::LogTargetsBasicFileLog logger{};
-  std::stringstream captured{};
-  logger.formatTo(std::ostreambuf_iterator{captured}, context, "{}", input);
+  std::string expect = logger.format(context, "{}", input);
   logger.log(context, "{}", input);
-  std::string_view expect = captured.view();
   std::ifstream logIn{logger.getLogPath()};
   if (!logIn.is_open()) {
     return cpplogger::test::ResultFail(
@@ -107,11 +84,9 @@ cpplogger::test::testFileLog(std::string_view input, Context context) {
             "I/O error while reading file '{}'",
             logger.getLogPath().generic_string()));
   }
-  if (actual != expect) {
+  if (actual != expect)
     return cpplogger::test::mismatch(expect, actual);
-  }
-  if (logIn.peek() != std::char_traits<char>::eof()) {
+  if (logIn.peek() != std::char_traits<char>::eof())
     return cpplogger::test::ResultFail("did not reach EOF");
-  }
   return {};
 }
